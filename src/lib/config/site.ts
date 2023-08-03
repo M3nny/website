b886f65288ccbd3bba72628922a1b00c72d6926c
M3nny <m3nny@protonmail.com>
2023-08-03 10:56:13 +0200

    Fix icons everywhere

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'localhost:5173',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: "M3nny's website",
+  subtitle: "Home",
   lang: 'en-US',
   description: 'descrizione',
   author: {
     avatar: '/assets/maskable@512.png',
     name: 'M3nny',
-    status: '🍵',
-    bio: 'prova prova'
+    status: '☕',
+    bio: 'I like trying out new stuff'
   },
   themeColor: '#3D4451'
 }
