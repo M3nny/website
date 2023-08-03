6ad034d5106055dd69de8ecb0856760e97eb717c
M3nny <m3nny@protonmail.com>
2023-08-03 10:34:59 +0200

    change icons

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'localhost:5173',
   title: 'Urara',
   subtitle: 'Sweet & Powerful SvelteKit Blog Template',
   lang: 'en-US',
